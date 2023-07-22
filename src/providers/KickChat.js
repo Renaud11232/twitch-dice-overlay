f20e4d90b39149510caec969ffa510f3daa75fc2
Renaud Gaspard <gaspardrenaud@hotmail.com>
2023-07-22 14:46:21 +0200

    added kick client implementation

--- src/providers/KickChat.js
+++ src/providers/KickChat.js
@@ -1,5 +1,16 @@
 import {Chat} from "./Chat";
+import {KickChatClient} from "../client/KickChatClient";
 
 export class KickChat extends Chat {
+
+    constructor(config) {
+        super(config);
+        this.client = new KickChatClient();
+        this.client.onMessage = data => {
+            console.log(data);
+        }
+        this.client.connectToChannels(config.kick.channels);
+    }
+
 
 }
