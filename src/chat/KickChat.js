bedd468b9301116474704992a0143547f03f1715
Renaud Gaspard <gaspardrenaud@hotmail.com>
2023-07-27 17:48:34 +0200

    kick chat now uses proper pusher client

--- src/chat/KickChat.js
+++ src/chat/KickChat.js
@@ -1,15 +1,18 @@
 import {AbstractChat} from "./AbstractChat";
-import {KickChatClient} from "../client/KickChatClient";
+import Pusher from "pusher-js";
 
 export class KickChat extends AbstractChat {
 
     constructor(config) {
         super(config);
-        this.client = new KickChatClient();
-        this.client.onMessage = data => {
-            console.log(data);
-        }
-        this.client.connectToChannels(config.kick.channels);
+        this.pusher = new Pusher("eb1d5f283081a78b932c", {
+            cluster: "us2",
+            enabledTransports: ["ws"]
+        });
+        const channel = this.pusher.subscribe(config.channel);
+        channel.bind("App\\Events\\ChatMessageEvent", data => {
+            this.handleMessage(data.content, data.sender.username);
+        });
     }
 
 
