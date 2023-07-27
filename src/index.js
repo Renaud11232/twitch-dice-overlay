9a9f80bd288612bbb01bf6ecab68abc8f805072c
Renaud Gaspard <gaspardrenaud@hotmail.com>
2023-07-27 17:01:39 +0200

    changed config structure

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import {Dice} from "./dice/Dice";
 import {Chat} from "./chat/Chat";
 
 fetchConfig().then(config => {
-    const dice = new Dice(config);
+    const dice = new Dice(config.dice);
 
-    const chat = Chat.getInstance(config);
+    const chat = Chat.getInstance(config.chat);
 
     chat.onRoll = function(rollValue) {
         dice.roll(`1d20@${rollValue}`);
